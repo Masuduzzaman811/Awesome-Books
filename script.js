63a8cea67d180e006d416294bfaf959f0d3262b4
Masuduzzaman Mohammad <masuduzzaman_811@outlook.com>
2022-06-29 20:13:11 +0600

    implement the AwesomeBooks class with addBook method

--- script.js
+++ script.js
@@ -1,55 +1,41 @@
-const books = [
-  {
-    id: 1,
-    title: 'The Power of your subconscious mind',
-    author: 'Joseph Morphy',
-  },
-];
-
-const bookListSection = document.querySelector('#book-list');
-
-function renderBookList(bookList) {
-  bookListSection.innerHTML = bookList
-    .map(
-      (book) => `<p class="title">${book.title}</p>
-            <p>${book.author}</p>
-            <button data-id=${book.id} class="remove">Remove</button>
-            <hr>`,
-    ).join('');
-}
-function saveBookToStorage(bookList) {
-  localStorage.setItem('bookList', JSON.stringify(bookList));
-}
-function getBookListFromLocalStorage() {
-  const bookListFromLocalStorage = localStorage.getItem('bookList');
-  if (bookListFromLocalStorage) {
-    return JSON.parse(bookListFromLocalStorage);
+class AwesomeBooks {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+    this.books = [];
   }
-  return books;
-}
-renderBookList(getBookListFromLocalStorage());
-const addBookForm = document.querySelector('#add-book');
-addBookForm.addEventListener('submit', function setVal(event) {
-  event.preventDefault();
-  const title = event.target.querySelector('#title').value;
-  const author = event.target.querySelector('#author').value;
-  const bookList = getBookListFromLocalStorage();
-  const id = bookList.length + 1;
-  bookList.push({
-    title,
-    author,
-    id,
-  });
-  this.reset();
-  renderBookList(bookList);
-  saveBookToStorage(bookList);
-});
-bookListSection.addEventListener('click', (event) => {
-  if (event.target.classList.contains('remove')) {
-    const { id } = event.target.dataset;
-    const bookList = getBookListFromLocalStorage();
-    const bookListFiltered = bookList.filter((book) => book.id !== +id);
-    renderBookList(bookListFiltered);
-    saveBookToStorage(bookListFiltered);
+
+  addBook(title, author) {
+    const id = this.books.length + 1;
+    const book = new AwesomeBooks(title, author, id);
+    this.books.push(book);
+    this.#save();
   }
-});
+
+  #addBooks(books) {
+    books.forEach((book) => {
+      this.addBook(book.title, book.author);
+    });
+  }
+
+  getBooks() {
+    return this.books;
+  }
+
+  #save() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  load() {
+    const books = JSON.parse(localStorage.getItem('books'));
+    if (books) {
+      this.#addBooks(books);
+    }
+  }
+
+  deleteBook(id) {
+    this.books = this.books.filter((book) => book.id !== id);
+    this.#save();
+  }
+}
